3c058d47afe017d06b0bdc363d91490c4d73ee02
yamamotokazutaka <owner@MA4-0014.local>
2019-04-29 19:01:24 +0900

    Imageimageimageimage4

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-   process resize_to_fit:[400, 400]
+  #include CarrierWave::MiniMagick
+   #process resize_to_fit:[400, 400]
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
