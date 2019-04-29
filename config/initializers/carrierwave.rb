fd26c6dcededcf6659aa670b859aca705131adc2
yamamotokazutaka <owner@MA4-0014.local>
2019-04-29 18:01:23 +0900

    Imageimageimageimage2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'imageimageimageimage'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/imageimageimageimage'
+  config.asset_host = 'https://imageimageimageimage.s3.amazonaws.com'
 end
